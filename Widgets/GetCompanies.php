08843824d2499aec15e07ead78a27357efde10b5
Romain Biard <romain.biard@gmail.com>
2019-12-30 12:55:00 +0100

    Render HTML directly

--- Widgets/GetCompanies.php
+++ Widgets/GetCompanies.php
@@ -88,9 +88,11 @@ class GetCompanies extends Widget
         // $idSite = Common::getRequestVar('idSite');
         // $site = new Site($idSite);
 
-        $output = "<h2>$this->translator->translate('IPtoCompany_CorrespondanceIPtoCompanies')</h2>";
+        $output = '<div class="widget-body">';
+        $output .= "<h2>$this->translator->translate('IPtoCompany_CorrespondanceIPtoCompanies')</h2>";
         $output .= "<br/>";
         $output .= "The answer to life is ";
+        $output .= "</div>";
 
         // return $this->renderTemplate('showCompanies', []);
         return $ouput;
