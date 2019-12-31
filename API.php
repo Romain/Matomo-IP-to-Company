60155174f10b09c47e6aab9790cd0b77a521ebb9
Romain Biard <romain.biard@gmail.com>
2019-12-31 16:34:42 +0100

    Fixed a bug in the method to generate the base URL

--- API.php
+++ API.php
@@ -92,7 +92,7 @@ class API extends \Piwik\Plugin\API
     private function getBaseUrl()
     {
         return sprintf(
-            "%s://%s%s",
+            "%s://%s",
             isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
             $_SERVER['SERVER_NAME']
         );
