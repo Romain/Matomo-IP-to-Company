5f387295b50955e0291cb44f185ae22d11b58e61
Romain Biard <romain.biard@gmail.com>
2019-12-31 15:42:43 +0100

    Created a report to be able to request the API

---
+++ API.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\IPtoCompany;
+
+use Piwik\DataTable;
+use Piwik\DataTable\Row;
+
+/**
+ * API for plugin IPtoCompany
+ *
+ * @method static \Piwik\Plugins\IPtoCompany\API getInstance()
+ */
+class API extends \Piwik\Plugin\API
+{
+
+    /**
+     * Another example method that returns a data table.
+     * @param int    $idSite
+     * @param string $period
+     * @param string $date
+     * @param bool|string $segment
+     * @return DataTable
+     */
+    public function getFoo($idSite, $period, $date, $segment = false)
+    {
+        $table = new DataTable();
+
+        $table->addRowFromArray(array(Row::COLUMNS => array('nb_visits' => 5)));
+
+        return $table;
+    }
+}
