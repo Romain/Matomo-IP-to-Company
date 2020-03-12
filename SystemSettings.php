1fd9b1c5cdc7ca61df7b916b8ab3f3a679e6d1d9
Romain Biard <romain.biard@gmail.com>
2020-03-12 11:20:33 +0100

    Create a parameter to define the lifetime of the cache for the results (in weeks)

--- SystemSettings.php
+++ SystemSettings.php
@@ -24,27 +24,32 @@ use Piwik\Validators\NotEmpty;
 class SystemSettings extends \Piwik\Settings\Plugin\SystemSettings
 {
     /** @var Setting */
-    public $metric;
+    public $ipInfoAccessToken;
 
     /** @var Setting */
-    public $browsers;
-
-    /** @var Setting */
-    public $description;
-
-    /** @var Setting */
-    public $password;
+    public $cacheLifeTimeForResults;
 
     protected function init()
     {
         // Create a setting to store the IPInfo API token
         $this->ipInfoAccessToken = $this->createIpInfoAccessTokenSetting();
+        $this->cacheLifeTimeForResults = $this->createCacheLifeTimeForResultsSetting();
     }
 
     private function createIpInfoAccessTokenSetting()
     {
         return $this->makeSetting('ipInfoAccessToken', $default = '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
             $field->title = Piwik::translate('IPtoCompany_YourIPInfoAccessToken');
+            $field->uiControl = FieldConfig::UI_CONTROL_TEXT;
+            $field->description = Piwik::translate('IPtoCompany_PasteYourAccessToken');
+            // $field->validators[] = new NotEmpty();
+        });
+    }
+
+    private function createCacheLifeTimeForResultsSetting()
+    {
+        return $this->makeSetting('cacheLifeTimeForResults', $default = 0, FieldConfig::TYPE_INT, function (FieldConfig $field) {
+            $field->title = Piwik::translate('IPtoCompany_LifeTimeOfCacheForResultsInWeeks');
             $field->uiControl = FieldConfig::UI_CONTROL_TEXT;
             $field->description = Piwik::translate('IPtoCompany_PasteYourAccessToken');
             // $field->validators[] = new NotEmpty();
