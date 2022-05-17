87cfc19563280872febe95febcd7c01bd3ba0d14
Romain Biard <romain.biard@gmail.com>
2022-05-17 12:29:42 +0200

    Added a GET parameter to the scheduled task to improve performances (#16)

--- Tasks.php
+++ Tasks.php
@@ -42,7 +42,8 @@ class Tasks extends \Piwik\Plugin\Tasks
             'idSite'    => $siteId,
             'period'    => 'day',
             // 'date'      => '2019-12-17'
-            'date'      => 'yesterday'
+            'date'      => 'yesterday',
+            'trigger'   => 'archivephp'
         ]);
 
         // Generate the HTML
